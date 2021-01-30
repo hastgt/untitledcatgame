efb4194acfa8f406986b3c8297895d7d3f3adb93
Dainis K <bonanzaa@inbox.lv>
2021-01-30 20:48:15 +0100

    LWRP lighting, Soul-clicking, UI, character float, Main Menu

---
+++ Assets/bonanzaaThings/GhostGlowFlunctuation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class GhostGlowFlunctuation : MonoBehaviour
+{
+    public float _minValue;
+    public float _maxValue;
+    public float _frequency;
+
+    public UnityEngine.Experimental.Rendering.Universal.Light2D light;
+
+    private float _offset;
+    void Start()
+    {
+        _offset = Random.Range(_minValue, _maxValue);
+        light = GetComponent<UnityEngine.Experimental.Rendering.Universal.Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sinWaveAdd = 1.7f;
+        float tempIntensity = Mathf.Abs(sinWaveAdd + Mathf.Sin(Time.fixedTime * Mathf.PI * _frequency)) * _offset;
+        light.intensity = tempIntensity;
+        
+    }
+}
---
+++ Assets/bonanzaaThings/UIcountingSouls.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIcountingSouls : MonoBehaviour
+{
+    Vector2 mousePos;
+    public Camera cam;
+
+    public int _soulsCollected = 0;
+
+    public TMP_Text _maxSoulText;
+    public TMP_Text _countText;
+    public Image _soulIcon;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero, Mathf.Infinity);
+            if (hit.collider != null)
+            {
+                hit.collider.gameObject.SetActive(false);
+                _soulsCollected++;
+                _countText.text = _soulsCollected.ToString();
+
+                StartCoroutine(FadeInText(_countText));
+                StartCoroutine(FadeInText(_maxSoulText));
+                StartCoroutine(FadeInImage(_soulIcon));
+                
+            }
+        }
+    }
+
+    private IEnumerator FadeInText(TMP_Text text)
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+        while (text.color.a < 1.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + (Time.deltaTime / 0.5f));
+            yield return null;
+        }
+        yield return new WaitForSeconds(2);
+        StartCoroutine(FadeOutText(text));
+        
+    }
+
+    private IEnumerator FadeOutText(TMP_Text text)
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        while (text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / 0.5f));
+            yield return null;
+        }
+    }
+
+    private IEnumerator FadeInImage(Image image)
+    {
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+        while (image.color.a < 1.0f)
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, image.color.a + (Time.deltaTime / 0.5f));
+            yield return null;
+        }
+        yield return new WaitForSeconds(2);
+        StartCoroutine(FadeOutImage(image));
+    }
+
+    private IEnumerator FadeOutImage(Image image)
+    {
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 1);
+        while (image.color.a > 0.0f)
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, image.color.a - (Time.deltaTime / 0.5f));
+            yield return null;
+        }
+    }
+}
